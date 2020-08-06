be02a3c59bc79c24e22777a1807e894119179bfb
Wesley-T-Wang <Wesley.Wang.T@gmail.com>
2020-08-05 17:50:24 -0700

    first commit

---
+++ Wesley's RockPaperScissors Game/TestFunction.py
@@ -0,0 +1,13 @@
+import ListString
+
+def test (throw):
+    
+    """test function"""
+    
+    testo = ListString.list_to_string(self, throw, " ")
+    assert isinstance(testo, String)
+    
+def testtoo (stringone, stringtwo):
+    testo = RockPaperScissorsGame.evaluate(stringone, stringtwo)
+    assert isinstance(testo, string)
+
